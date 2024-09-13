a757ea4fd8dd80b6b292b4283107131344e4f22b
anouvene <nouveno@gmail.com>
2024-09-13 16:10:18 +0200

    test.yml updated

--- test/admin/test_login.py
+++ test/admin/test_login.py
@@ -18,7 +18,7 @@ class TestLogin:
         # Se connecter
         email_input.send_keys("a.nouvene@it-students.fr")
         time.sleep(1)
-        password_input.send_keys("jeTeste24$")
+        password_input.send_keys("azerty82$")
         time.sleep(1)
         login_button.click()
 
