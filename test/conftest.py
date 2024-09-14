1061b2bd37f50d8e6720855cbd18704a62046043
anouvene <nouveno@gmail.com>
2024-09-14 18:03:06 +0200

    Updated Service executable_path

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService, Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -13,12 +13,12 @@ def driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-debugging-port=9222")
 
-    options.add_argument('--dns-prefetch-disable')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--enable-cdp-events')
+    # options.add_argument('--dns-prefetch-disable')
+    # options.add_argument('--disable-gpu')
+    # options.add_argument('--enable-cdp-events')
 
     #options.binary_location = "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome/"
-    driver_path = Service('/usr/local/bin/chromedriver-mac-arm64/chromedriver')
+    driver_path = ChromeService(executable_path='/usr/local/bin/chromedriver-mac-arm64/chromedriver')
 
 
     # service = Service(ChromeDriverManager().install())
@@ -28,7 +28,7 @@ def driver():
 
     driver = webdriver.Chrome(service=driver_path, options=options)
 
-    driver.implicitly_wait(10)
+    # driver.implicitly_wait(10)
     yield driver
     driver.quit()
 
