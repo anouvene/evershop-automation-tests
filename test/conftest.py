2ef826b4110e3f5943c79d23e97ab48c7853498a
anouvene <nouveno@gmail.com>
2024-09-14 16:44:15 +0200

    Conftest updated

--- test/conftest.py
+++ test/conftest.py
@@ -8,10 +8,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-search-engine-choice-screen")
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-debugging-port=9222")
+
+    options.add_argument('--dns-prefetch-disable')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--enable-cdp-events')
 
     # service = Service(ChromeDriverManager().install())
     # driver = webdriver.Chrome(service=service, options=options)
