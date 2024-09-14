cebad22418498269693978d60a342628b815639a
anouvene <nouveno@gmail.com>
2024-09-14 23:47:59 +0200

    self.driver added

--- test/admin/test_products.py
+++ test/admin/test_products.py
@@ -12,20 +12,20 @@ class TestProducts(TestLogin):
         try:
             # Wait for and click on the Catalog menu item
             time.sleep(10)
-            catalog_menu = driver.find_element((By.XPATH, "//span[text()='Catalog']"))
+            catalog_menu = self.driver.find_element((By.XPATH, "//span[text()='Catalog']"))
             catalog_menu.click()
 
             # Wait for and click on the Products menu item
             time.sleep(15)
-            products_menu = driver.find_element(By.XPATH, "//a[contains(@href, '/admin/products') and contains(., 'Products')]")
+            products_menu = self.driver.find_element(By.XPATH, "//a[contains(@href, '/admin/products') and contains(., 'Products')]")
             products_menu.click()
 
             # Wait for the Products page heading to appear and verify its visibility
             time.sleep(15)
-            page_heading = driver.find_element(By.XPATH, "//h1[text()='Products']")
+            page_heading = self.driver.find_element(By.XPATH, "//h1[text()='Products']")
             assert page_heading.is_displayed(), "Products page did not load correctly"
 
         except TimeoutException as e:
             print(f"TimeoutException encountered: {e}")
-            driver.save_screenshot('screenshot.png')  # Save a screenshot for further debugging
+            self.driver.save_screenshot('screenshot.png')  # Save a screenshot for further debugging
             raise
--- test/conftest.py
+++ test/conftest.py
@@ -4,11 +4,11 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-@pytest.fixture(scope='class')
+@pytest.fixture()
 def driver(request):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--disable-search-engine-choice-screen")
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--remote-debugging-port=9222")
